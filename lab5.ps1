4c6258ecb9fc3e269c4fe3a8788175d369f76c12
200454885 <94725801+200454885@users.noreply.github.com>
2021-11-19 20:08:38 -0500

    Add files via upload

---
+++ lab5.ps1
@@ -0,0 +1,12 @@
+﻿Param ( [switch]$System, [switch]$Disks,
+        [switch]$Network)
+
+Import-Module Muskan
+
+if ($System -eq $false -and $Disks -eq $false -and $Network -eq $false) {
+      Muskan-System; Muskan-Disks; Muskan-Network;
+} else {
+      if ($System) {Muskan-System;}
+      if ($Disks) {Muskan-Disks;}
+      if ($Network) {Muskan-Network;}
+}
